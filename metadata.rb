4c2de47f6aab1e56555a3c040406008314ccb46b
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2012-10-05 10:59:55 +0100

    started

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "National Theatre"
+maintainer_email "jdrawneek@nationaltheatre.org.uk"
+license          "All rights reserved"
+description      "Installs/Configures webserver-chef"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "php", ">= 1.1.0"
+depends          "apache2"
+depends          "imagemagick"
