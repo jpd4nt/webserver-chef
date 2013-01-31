24f15df9a18fef4a82055c61b41d4ee798c3f95d
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2013-01-31 16:42:18 +0000

    enable it for redhat linux

--- recipes/mongodb_client.rb
+++ recipes/mongodb_client.rb
@@ -12,3 +12,17 @@ php_pear "mongo" do
   action :install
   notifies :restart, "service[apache2]", :delayed
 end
+
+# This means that the socket could not be opened due to permissions issues.
+# On Red Hat variants, this can be caused by a default setting that does not
+# allow Apache to create network connections.
+case node['platform_family']
+when "rhel"
+  selinux = `/usr/sbin/getsebool httpd_can_network_connect`
+  execute "selinx_http" do
+    command "/usr/sbin/setsebool -P httpd_can_network_connect 1"
+    action :run
+    notifies :restart, "service[apache2]", :delayed
+    not_if {selinux.eql? "httpd_can_network_connect --> on"}
+  end
+end
