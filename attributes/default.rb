4d0121d68a16d6e1160c1867895645be1574b9ed
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-04-03 18:23:47 +0100

    fixes for amazon linux and httpd24

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,6 +20,12 @@ when "rhel", "fedora", "centos"
   case node['platform']
   when "amazon"
     default['php']['packages'] = %w{ php54 php54-devel php54-cli php54-gd php54-mbstring php54-mysqlnd php54-pecl-igbinary php54-xml php54-soap php54-dba php54-mcrypt }
+    default['apache']['package'] = 'httpd24'
+    default['apache']['default_modules'] = %w[
+      status alias auth_basic authn_file authz_core authz_groupfile authz_host authz_user autoindex
+      dir env mime negotiation setenvif
+    ]
+    default['mysql']['server']['packages'] = %w[mysql-server]
   when "rhel"
     default['php']['packages'] = %w{ php54 php54-php php54-runtime php54-php-devel php54-php-cli php54-php-gd php54-php-mbstring php54-php-mysqlnd php54-php-pecl-apc php54-php-xml php54-php-soap php54-php-dba }
     default['php']['pear']         = '/opt/rh/php54/root/usr/bin/pear'
