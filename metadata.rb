50dc2f3582f239a035fd2eedf61bf4a370d11e17
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-06-10 17:53:03 +0100

    update meta version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jdrawneek@nationaltheatre.org.uk"
 license          "All rights reserved"
 description      "Installs/Configures webserver-chef"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.6"
+version          "0.1.7"
 depends          "php", ">= 1.1.0"
 depends          "apache2"
 depends          "imagemagick"
