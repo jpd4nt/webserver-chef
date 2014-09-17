4c43aa1b055b1d49dce1561372edde7c3e9124d2
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-09-17 16:20:21 +0100

    fix thedeck

--- recipes/thedeck.rb
+++ recipes/thedeck.rb
@@ -11,7 +11,7 @@ include_recipe "build-essential::default"
 include_recipe "webserver-chef::default"
 
 remote_file "/tmp/#{node['php']['geos']['version'}.tar.bz2" do
-  source "http://download.osgeo.org/geos/#{node['php']['geos']['version'}.tar.bz2"
+  source "http://download.osgeo.org/geos/#{node['php']['geos']['version']}.tar.bz2"
   mode "0644"
   not_if { ::File.exists?(node['php']['ext_dir'] . '/geos.so') }
 end
@@ -19,14 +19,14 @@ end
 execute "install-geoPHP" do
   cwd "/tmp"
   command <<-EOF
-    tar jxf #{node['php']['geos']['version'}.tar.bz2
-    cd #{node['php']['geos']['version'}
+    tar jxf #{node['php']['geos']['version']}.tar.bz2
+    cd #{node['php']['geos']['version']}
     ./configure --enable-php && make clean && make
     make install
     ldconfig
     cd ..
-    rm -yr #{node['php']['geos']['version'}
-    rm -y #{node['php']['geos']['version'}.tar.bz2
+    rm -yr #{node['php']['geos']['version']}
+    rm -y #{node['php']['geos']['version']}.tar.bz2
   EOF
   not_if { ::File.exists?(node['php']['ext_dir'] . '/geos.so') }
 end
