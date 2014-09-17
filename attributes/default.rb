ee8c918b4d0e8a9e735bd4054bb491da85b80b28
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-09-17 16:43:04 +0100

    fix attrib

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,11 +34,11 @@ when "rhel", "fedora", "centos"
     default['php']['php54'] = %w{ php54 php54-php php54-runtime php54-php-devel php54-php-cli php54-php-gd php54-php-mbstring php54-php-mysqlnd php54-php-pecl-apc php54-php-xml php54-php-soap php54-php-dba }
     default['php']['php55'] = %w{ php55 php55-php php55-runtime php55-php-devel php55-php-cli php55-php-gd php55-php-mbstring php55-php-mysqlnd php55-php-xml php55-php-soap php55-php-dba php55-php-pecl-jsonc php55-php-pecl-memcache }
     default['php']['packages'] = node['php'][node['php']['version']]
-    default['php']['pear']         = '/opt/rh/php54/root/usr/bin/pear'
-    default['php']['pecl']         = '/opt/rh/php54/root/usr/bin/pecl'
-    default['php']['bin']          = '/opt/rh/php54/root/usr/bin/php'
-    default['php']['ext_conf_dir'] = '/opt/rh/php54/root/etc/php.d'
-    default['php']['ext_dir']      = '/opt/rh/php54/root/usr/lib64/php/modules'
+    default['php']['pear']         = "/opt/rh/#{node['php']['version']}/root/usr/bin/pear"
+    default['php']['pecl']         = "/opt/rh/#{node['php']['version']}/root/usr/bin/pecl"
+    default['php']['bin']          = "/opt/rh/#{node['php']['version']}/root/usr/bin/php"
+    default['php']['ext_conf_dir'] = "/opt/rh/#{node['php']['version']}/root/etc/php.d"
+    default['php']['ext_dir']      = "/opt/rh/#{node['php']['version']}/root/usr/lib64/php/modules"
   else
     default['php']['packages'] = %w{ php54 php54-php php54-runtime php54-php-devel php54-php-cli php54-php-gd php54-php-mbstring php54-php-mysqlnd php54-php-xml php54-php-soap php54-php-dba php54-php-pecl-igbinary }
     default['php']['pear']         = '/opt/rh/php54/root/usr/bin/pear'
