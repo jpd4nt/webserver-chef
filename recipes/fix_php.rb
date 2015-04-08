199889a53a3b3f8797028ba85e6b9f79a0c06389
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2015-04-08 16:19:53 +0100

    fix php due to apache cookbook change

---
+++ recipes/fix_php.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: webserver-chef
+# Recipe:: fix_php
+#
+# Copyright 2014, National Theatre
+#
+# All rights reserved - Do Not Redistribute
+#
+
+case node['platform_family']
+when "rhel", "fedora", "centos"
+  cookbook_file "php54-php.conf" do
+    path "/etc/httpd/conf-enabled/php.conf"
+    action :create_if_missing
+    notifies :restart, "service[apache2]", :delayed
+  end
+end
